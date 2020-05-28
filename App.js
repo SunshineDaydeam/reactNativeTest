eb8850d3cfc1005677458436c47f531a51589a90
Sunshine Daydream <alexbruner@live.com>
2020-05-28 16:34:22 -0500

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  let counter = 1;
+  console.log('rendered')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.adwhaleLogo}>I am an app</Text>
+      <Text>{counter}</Text>
+      <Text>Alex's first app</Text>
+      <Button 
+        title="Add"
+        onPress={e => console.log('clicked')}
+      />
     </View>
   );
 }
@@ -16,4 +24,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  adwhaleLogo: {
+    color: "blue",
+    fontSize: 60
+  }
 });
